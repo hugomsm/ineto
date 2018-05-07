5e585297b8499783bac49ec819f92f38c4ccc716
hugomsm <hugomaiadasilvamarques@gmail.com>
2018-05-07 12:02:35 -0300

    Initial commit

---
+++ src/components/AulaItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <q-collapsible icon="explore" label="First">
+    <div>
+      {{aula.descricao}}
+    </div>
+  </q-collapsible>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['aula'],
+  // name: 'ComponentName',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/menuprincipal.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page padding>
+
+    <q-layout-header reveal="true" v-model="header">
+      iNeto
+    </q-layout-header>
+    
+    <q-list>
+      <aulaItem v-for="aula in aulas" v-bind:key="aula.idAula" :aula="aula" ></aulaItem>
+    </q-list>
+
+  <q-layout-footer v-model="footer">
+      <q-btn
+      v-back-to-top.animate="{offset: 250, duration: 200}"
+      round
+      color="primary"
+      class="fixed-bottom-right animate-pop"
+      style="margin: 0 15px 15px 0"
+      >
+       <q-icon name="keyboard_arrow_up" />
+      </q-btn>
+  </q-layout-footer>
+
+
+    </q-page>
+</template>
+
+<script>
+import { AulaItem } from "../components/AulaItem.vue";
+
+export default {
+  data: function() {
+    return {
+      header: "iNeto",
+      aulas: [
+        {
+          idAula: 1,
+          tituloAula: "Aula 1 - Introdução",
+          descricao: "Entenda os motivos de aprender sobre tecnologia"
+        },
+        {
+          idAula: 2,
+          tituloAula: "Aula 2 - Conheça seu aparelho",
+          descricao: "Conheça as funcionalidades básicas"
+        }
+      ]
+    };
+  },
+  components:{
+    aulaItem: AulaItem
+  }
+};
+</script>
+
+<style>
+
+</style>
