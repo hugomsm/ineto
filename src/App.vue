acc09232dd6f02f35c48edc0623a4a5a779c0b50
hugomsm <hugomaiadasilvamarques@gmail.com>
2018-05-11 14:41:34 -0300

    travou-tela-retrato

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,16 @@
 </template>
 
 <script>
+
+window.screen.orientation.lock('portrait');
+
+/* function feedback() {
+  const DF = window.plugins.deviceFeedback;
+  DF.haptic(DF.VIRTUAL_KEY);
+} */
+
+// this.$root.$on('deviceFeedback', feedback);
+
 export default {
   name: 'App',
 };
