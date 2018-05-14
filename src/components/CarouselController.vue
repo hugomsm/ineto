fb00f252b2e42f485184c4a22b0d1a4707e97646
hugomsm <hugomaiadasilvamarques@gmail.com>
2018-05-14 11:56:11 -0300

    slot

--- src/components/CarouselController.vue
+++ src/components/CarouselController.vue
@@ -1,46 +1,59 @@
 <template>
-  <div class='fixed-bottom q-ml-md q-mb-md'>
+<div style="display: grid; grid-template-rows: 1fr auto; height: calc(100vh - 48px);">
+  <q-carousel class="text-black q-ma-md bg-white" v-model='slide'>
+    <slot></slot>
+  </q-carousel>
+  <!-- <div class='q-ml-md q-mb-md'> -->
+  <div class='row gutter-sm q-pa-md'>
+    <div class='col-6'>
             <q-btn
-            size='xl'
-            color='red'
-            push
-            @click='previousSlide'>
+           class='full-width'
+           size='xl'
+           color='red'
+           push
+           @click='slide = slide - 1'>
                 Anterior
-            </q-btn>
+          </q-btn>
+    </div>
+    <div class='col-6'>
             <q-btn
-            size='xl'
-            color='green'
-            push
-            @click='nextSlide'>
+           class='full-width'
+           size='xl'
+           color='green'
+           push
+           @click='slide = slide + 1'>
                 Próximo
             </q-btn>
+    </div>
+    <div class='col-12'>
             <q-btn
-            class='text-black'
-            size='xl'
-            color='light-blue-1'
-            push
-            @click='navigateToHome'
-            >
+           class='text-black full-width'
+           size='xl'
+           color='light-blue-1'
+           push
+           @click='navigateToHome'
+           >
                 Voltar ao início
             </q-btn>
     </div>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  props: ['relativeSlide'],
   data() {
+    return {
+      slide: 0,
+    };
   },
   methods: {
     navigateToHome() {
       this.$router.push('/');
     },
-    nextSlide() {
-      this.relativeSlide = this.relativeSlide + 1;
-    },
-    previousSlide() {
-      this.relativeSlide = this.relativeSlide - 1;
-    },
   },
 };
 </script>
+
+<style>
+</style>
--- src/pages/aula1.vue
+++ src/pages/aula1.vue
@@ -2,10 +2,12 @@
   <q-layout>
     <q-page-container >
         <q-page class='bg-light-blue-9'>
-            <q-carousel class="text-black q-ma-md bg-white" v-model="slide">
+            <carousel-controller>
                 <q-carousel-slide class="bg-white full-height">
                     <h2 class='text-center text-weight-medium'>
-                        Bem vindo ao iNeto! Clique em Próximo para avançar.
+                        Bem vindo ao iNeto! Clique no botão
+                        <p class='text-green'>
+                        Próximo</p> (abaixo) para continuar a aula.
                     </h2>
                 </q-carousel-slide>
                 <q-carousel-slide class="bg-secondary">
@@ -14,39 +16,14 @@
                 <q-carousel-slide class="bg-tertiary">
                     Slide 3
                 </q-carousel-slide>
-            </q-carousel>
-            <!-- <div class='fixed-bottom q-ml-md q-mb-md'>
-            <q-btn
-            size='xl'
-            color='red'
-            push
-            @click='slide = slide - 1'>
-                Anterior
-            </q-btn>
-            <q-btn
-            size='xl'
-            color='green'
-            push
-            @click='slide = slide + 1'>
-                Próximo
-            </q-btn>
-            <q-btn
-            class='text-black'
-            size='xl'
-            color='light-blue-1'
-            push
-            @click='navigateToHome'
-            >
-                Voltar ao início
-            </q-btn>
-    </div> -->
-            <carousel-controller :relativeSlide='slide'></carousel-controller>
+            </carousel-controller>
         </q-page>
     </q-page-container>
   </q-layout>
 </template>
 
 <style>
+
 </style>
 
 <script>
@@ -54,16 +31,6 @@
 import CarouselController from '../components/CarouselController.vue';
 
 export default {
-  data() {
-    return {
-      slide: 0,
-    };
-  },
-  methods: {
-    navigateToHome() {
-      this.$router.push('/');
-    },
-  },
   components: {
     carouselController: CarouselController,
   },
@@ -71,9 +38,5 @@ export default {
 </script>
 
 <style>
-    .q-carousel{
-        position: fixed;
-        height: 65%;
-    }
 </style>
 
